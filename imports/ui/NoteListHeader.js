45565e8c14252435194eecb52a69ec271922fa30
Amandeep <amandeepgupta1@gmail.com>
2017-04-10 01:21:41 +0530

    Style note list

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -5,8 +5,8 @@ import { createContainer } from 'meteor/react-meteor-data';
 
 export const NoteListHeader = (props) => {
   return (
-    <div>
-      <button onClick={() => {
+    <div className="item-list__header">
+      <button className="button" onClick={() => {
         props.meteorCall('notes.insert', (err, res) => {
           if (res) {
             props.Session.set('selectedNoteId', res);
